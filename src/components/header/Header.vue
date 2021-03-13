249f0991dd067ea61ff1a2a70ccb2492ab78fdb3
Marco Agostinelli <51815533+marcoMilkman@users.noreply.github.com>
2021-03-13 18:26:07 +0100

    setup

---
+++ src/components/header/Header.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="container">
+		<div class="leftContent">
+			<div class="logo"></div>
+		</div>
+		<div class="rightContent">
+			<ul>
+				<li v-for="item in items" :key="item.name">
+					<a href="javascritp:void(0)">
+						{{ item.name }}
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+		props: {
+			items: Array
+		}
+	}
+</script>
+<style>
+	.container {
+		height: 48px;
+		display: flex;
+		padding: 16px 0;
+		align-items: center;
+	}
+
+	.leftContent {
+		width: 50%;
+	}
+
+	.rightContent {
+		width: 50%;
+		padding-right: 16px;
+	}
+
+	ul {
+		list-style: none;
+		display: flex;
+		align-items: center;
+		height: 100%;
+		justify-content: flex-end;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		font-size: 12px;
+		color: white;
+		padding: 4px 16px;
+		margin-right: 8px;
+		text-transform: uppercase;
+		border: 1px solid white;
+		border-radius: 16px;
+		text-align: center;
+		cursor: pointer;
+		background-color: transparent;
+		transition: background-color .3s ease-in;
+	}
+
+	li:hover {
+		background-color:white;
+		color: rgb(46,46,46);
+	}
+	
+	li:last-child {
+		margin-right: 0;
+	}
+
+	.logo {
+		height: 32px;
+		width: 32px;
+		border-radius: 50%;
+		background: rgb(237,237,237);
+		background: linear-gradient(180deg, rgba(237,237,237,1) 15%, rgba(52,52,52,1) 100%);
+		margin-left: 16px;
+		opacity: .7;
+	}
+</style>
---
+++ src/components/wrapper/ContactPageWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<Header v-bind:items="items"></Header>
+	</div>
+</template>
+
+<script>
+import Header from '../header/Header';
+
+export default {
+	name: 'ContactPageWrapper',
+	components: { Header },
+	props: {
+		msg: String
+	},
+	data: () => ({
+		items: [
+			{ name: 'About' },
+			{ name: 'Shop' },
+			{ name: 'Cart' }
+		]
+	})
+}
+</script>
+<style>
+		@import './style.css';
+</style>
+
+
